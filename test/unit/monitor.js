3bff9ccb11627e78960bedba28928066f3523dc4
Christoph Tavan <dev@tavan.de>
2012-08-07 14:45:02 +0200

    Add support for gauges, timers and histograms in addition to meters.

--- test/unit/monitor.js
+++ test/unit/monitor.js
@@ -4,11 +4,23 @@ var url = 'plaintext://localhost:2003/';
 var interval = 500;
 
 exports['monitor.update()'] = function(test) {
-  monitor.connect(url, interval, function(err, monitor) {
+  var connect = monitor.factory(url, interval);
+  connect(function(err, monitor) {
+    monitor.gauge('group', 'gauge.name.' + 'test', function() {
+      return Date.now();
+    });
+
     monitor.update('group', 'name.' + 'test', Math.random() * 10);
     monitor.update('group', 'name.' + 'test', Math.random() * 10);
     monitor.update('group', 'name.' + 'test', Math.random() * 10);
     monitor.update('group', 'name.' + 'test', Math.random() * 10);
+
+    monitor.updateTimer('group', 'timer.name.' + 'test', Math.random() * 10);
+    monitor.updateHistogram('group', 'histogram.name.' + 'test',
+                            Math.random() * 10);
+    monitor.mark('group', 'meter.name.' + 'test');
+    monitor.mark('group', 'meter.name.' + 'test', Math.random() * 10);
+
     setTimeout(function() {
       monitor.end();
       test.done();
